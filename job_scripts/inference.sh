d0c0262ea366a73e4391bb1f74181b75302ea10e
Zonghuan Li <z.li-25@tudelft.nl>
2025-11-28 17:08:01 +0100

    change qos to medium

--- job_scripts/inference.sh
+++ job_scripts/inference.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=insy,general # Request partition. Default is 'general' 
-#SBATCH --qos=short         # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
+#SBATCH --qos=medium         # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
 #SBATCH --time=24:00:00      # Request run time (wall-clock). Default is 1 minute
 #SBATCH --cpus-per-task=4
 #SBATCH --ntasks=1          # Request number of parallel tasks per job. Default is 1
