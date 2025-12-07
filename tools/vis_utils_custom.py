27f1da7be4a379e87530da01e8b7ebd63507af0e
Kevinlzh9802 <zonghuan_li@outlook.com>
2025-12-07 09:55:23 +0100

    check none before plotting

--- tools/vis_utils_custom.py
+++ tools/vis_utils_custom.py
@@ -15,8 +15,10 @@ def kp_check(filename, kps):
 
 def plot_bboxes_kps(img, bboxes, kps):
     img_with_bboxes_kps = img.copy()
-    for bbox in bboxes:
-        cv2.rectangle(img_with_bboxes_kps, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 0, 255), 2)
-    for kp in kps:
-        cv2.circle(img_with_bboxes_kps, (int(kp[0]), int(kp[1])), 5, (0, 255, 0), -1)
+    if bboxes is not None:
+        for bbox in bboxes:
+            cv2.rectangle(img_with_bboxes_kps, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (0, 0, 255), 2)
+    if kps is not None:
+        for kp in kps:
+            cv2.circle(img_with_bboxes_kps, (int(kp[0]), int(kp[1])), 5, (0, 255, 0), -1)
     return img_with_bboxes_kps
