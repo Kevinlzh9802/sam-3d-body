874368bd0fe1d76050326765d685fc0550528445
Zonghuan Li <z.li-25@tudelft.nl>
2025-12-08 16:43:35 +0100

    reduce inference_test resources

--- job_scripts/inference_test.sh
+++ job_scripts/inference_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --partition=insy,general # Request partition. Default is 'general' 
-#SBATCH --qos=medium         # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
-#SBATCH --time=34:00:00      # Request run time (wall-clock). Default is 1 minute
-#SBATCH --cpus-per-task=4
+#SBATCH --qos=short         # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
+#SBATCH --time=2:00:00      # Request run time (wall-clock). Default is 1 minute
+#SBATCH --cpus-per-task=2
 #SBATCH --ntasks=1          # Request number of parallel tasks per job. Default is 1
-#SBATCH --mem=16G
+#SBATCH --mem=8G
 #SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
 #SBATCH --output=/home/nfs/zli33/slurm_outputs/sam_3d_body/slurm_%j.out # Set name of output log. %j is the Slurm jobId
 #SBATCH --error=/home/nfs/zli33/slurm_outputs/sam_3d_body/slurm_%j.err # Set name of error log. %j is the Slurm jobId
